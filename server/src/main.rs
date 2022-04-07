824a88442cee8986da3924a6e7a7681b1ecb1d21
Bob Rust <turbofish5@gmail.com>
2022-04-08 00:10:46 +0200

    Add main crabbies

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{response::IntoResponse, routing::get, Router};
+use clap::Parser;
+use std::net::{IpAddr, Ipv6Addr, SocketAddr};
+use std::str::FromStr;
+
+// Setup the command line interface with clap.
+#[derive(Parser, Debug)]
+#[clap(name = "server", about = "A server for our wasm project!")]
+struct Opt {
+    /// set the listen addr
+    #[clap(short = 'a', long = "addr", default_value = "::1")]
+    addr: String,
+
+    /// set the listen port
+    #[clap(short = 'p', long = "port", default_value = "8080")]
+    port: u16,
+}
+
+#[tokio::main]
+async fn main() {
+    let opt = Opt::parse();
+
+    let app = Router::new().route("/", get(hello));
+
+    let sock_addr = SocketAddr::from((
+        IpAddr::from_str(opt.addr.as_str()).unwrap_or(IpAddr::V6(Ipv6Addr::LOCALHOST)),
+        opt.port,
+    ));
+
+    println!("listening on http://{}", sock_addr);
+
+    axum::Server::bind(&sock_addr)
+        .serve(app.into_make_service())
+        .await
+        .expect("Unable to start server");
+}
+
+async fn hello() -> impl IntoResponse {
+    "hello from server!"
 }
